6c2052702c2274d249f3f865d1bc18ed93ab68c0
yasir-akkaya <yasirakkaya@outlook.com>
2024-05-28 09:34:15 +0300

    mini budget project

---
+++ Budgeting_MiniProject.php
@@ -0,0 +1,60 @@
+<?php
+
+//Bu projede, farklı dosyalardan alınan gelir,gider,tarih ve açıklama bilgileri ile bütçe hesaplaması yapılacaktır.
+
+$file_path_counter = 1;
+$BudgetDatas = array();
+$income = 0;
+$expense = 0;
+
+while (file_exists("Budget_" . $file_path_counter . ".txt")) {
+    $fileName = "Budget_" . $file_path_counter . ".txt";
+    $file = fopen($fileName, "r");
+
+    while (($line = fgetcsv($file)) !== false) {
+        array_push($BudgetDatas, $line);
+    }
+    $file_path_counter++;
+}
+
+// array zamana göre sırala
+usort($BudgetDatas, fn($a, $b) => strtotime($a[0]) <=> strtotime($b[0]));
+
+
+
+echo "<pre>";
+print_r($BudgetDatas);
+echo "<pre>";
+
+$lineNumber = count($BudgetDatas);
+
+
+echo "<h1>Budget</h1>";
+
+echo "<table>";
+echo "<tr>";
+echo "<th>Date</th>";
+echo "<th>Description</th>";
+echo "<th>Amount</th>";
+echo "</tr>";
+
+for ($i = 0; $i < $lineNumber; $i++) {
+
+    if ($BudgetDatas[$i][2] > 0) {
+        $income += $BudgetDatas[$i][2];
+    } else {
+        $expense += $BudgetDatas[$i][2];
+    }
+    echo "<tr>";
+    echo "<td>" . $BudgetDatas[$i][0] . "</td>";
+    echo "<td>" . $BudgetDatas[$i][1] . "</td>";
+    echo "<td>" . $BudgetDatas[$i][2] . "</td>";
+    echo "</tr>";
+}
+echo "<tr>";
+echo "<td> Income: " . $income . "$</td>";
+echo "<td> Expense: " . $expense. "$</td>";
+echo "<td> Balance: " . $income+ $expense . "$</td>";
+echo "</tr>";
+
+echo "</table>";
--- May27_1_index.php
+++ May27_1_index.php
@@ -4,3 +4,46 @@
 
 // echo 1;
 
+echo '<pre>';
+print_r(scandir(__DIR__));
+echo '</pre>';
+
+// mkdir('denemeDir/altDenemeDir',recursive:true);
+
+// rmdir('denemeDir/altDenemeDir');
+
+// rmdir('denemeDir');
+
+// if (file_exists('file.txt')) {
+//     echo filesize('file.txt') . '<br>';
+    
+//     file_put_contents('file.txt', 'Hello World');
+//     clearstatcache();
+//     echo filesize('file.txt');
+
+// } else {
+//     echo "Fİle does not exist";
+// }
+
+if (('file.txt')) {
+    echo file_get_contents('file.txt');
+    
+} else {
+    echo "File does not exist";
+}
+
+// if(file_exists('file.txt')) {
+//     $file=fopen('file.txt', 'r');
+// } else {
+//     echo "File does not exist";
+//     return;
+// }
+
+// while(($line = fgets($file))!==false) {
+//     echo $line . '<br>';
+// }
+// fclose($file);
+
+file_put_contents('file.txt', 'Hello World', FILE_APPEND);
+
+print_r( pathinfo('file.txt'));
